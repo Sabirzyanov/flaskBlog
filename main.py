0c90350285177a3bbd69aa7fc237cd402a35f2f4
Sabirzyanov <57670787+Sabirzyanov@users.noreply.github.com>
2020-04-11 21:03:00 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -118,6 +118,5 @@ def news_output_admin(id):
 
 
 if __name__ == '__main__':
-    # port = int(os.environ.get("PORT", 5000))
-    # app.run(host='0.0.0.0', port=port)
-    app.run()
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
